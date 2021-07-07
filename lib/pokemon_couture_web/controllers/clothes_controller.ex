33381abdeea6d6b69f54a6d6d5c84c4bf27dcb6c
Lili <pastrnakie@seznam.cz>
2021-07-07 19:08:29 +0200

    add shop name to lists in clothes index

--- lib/pokemon_couture_web/controllers/clothes_controller.ex
+++ lib/pokemon_couture_web/controllers/clothes_controller.ex
@@ -4,9 +4,22 @@ defmodule PokemonCoutureWeb.ClothesController do
   alias PokemonCouture.Shops
   alias PokemonCouture.Shops.Clothes
 
+  def create_shop_map(clothes, map) do
+    case map[clothes.location] do
+      nil ->
+        Map.put(map, clothes.location, [clothes])
+      list_of_clothes when is_list(list_of_clothes) ->
+        Map.put(map, clothes.location, list_of_clothes ++ [clothes])
+    end
+  end
+
   def index(conn, _params) do
     clothes = Shops.list_clothes()
-    render(conn, "index.html", clothes: clothes)
+    clothes_map =
+      clothes
+      |> Enum.reduce(%{}, &create_shop_map/2)
+    # IO.inspect(clothes_map)
+    render(conn, "index.html", clothes: clothes, clothes_map: clothes_map)
   end
 
   def new(conn, _params) do
