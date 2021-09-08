85a31d3d0242541d740755a1d0c0ce2175b3666c
Lili <pastrnakie@seznam.cz>
2021-09-08 16:49:52 +0200

    lightswitch done

--- lib/pokemon_couture_web/live/clothes_tracker_live.ex
+++ lib/pokemon_couture_web/live/clothes_tracker_live.ex
@@ -2,7 +2,12 @@ defmodule PokemonCoutureWeb.ClothesTrackerLive do
   use PokemonCoutureWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :light_bulb_status, "off")
+    socket =
+      socket
+      |> assign(:light_bulb_status, "off")
+      |> assign(:on_button_status, "")
+      |> assign(:off_button_status, "disabled")
+
     {:ok, socket}
   end
 
@@ -10,7 +15,19 @@ defmodule PokemonCoutureWeb.ClothesTrackerLive do
     socket =
       socket
       |> assign(:light_bulb_status, "on")
+      |> assign(:on_button_status, "disabled")
+      |> assign(:off_button_status, "enabled")
 
     {:noreply, socket}
   end
+
+  def handle_event("off", _value, socket) do
+    socket =
+      socket
+      |> assign(:light_bulb_status, "off")
+      |> assign(:on_button_status, "")
+      |> assign(:off_button_status, "disabled")
+
+    {:noreply, socket}
+    end
 end
