ee21d34e028aec8824240673bb5abb3472bf58c6
Lili <pastrnakie@seznam.cz>
2021-10-20 14:33:15 +0200

    syntax fix

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :logger, level: :info
 
 # sentry config
 config :sentry,
-  dsn: System.get_env("SENTRY_DSN") || ""
+  dsn: System.get_env("SENTRY_DSN") || "",
   environment_name: :prod,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
